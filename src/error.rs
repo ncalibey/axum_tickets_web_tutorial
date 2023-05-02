56457448e0b159d99da3066ed28479fb02b5a3cf
Nick Calibey <nick.calibey@gmail.com>
2023-05-02 08:16:45 -0500

    Init commit

---
+++ src/error.rs
@@ -0,0 +1,76 @@
+use axum::{response::{IntoResponse, Response}, http::StatusCode};
+use serde::Serialize;
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
+pub enum Error {
+    LoginFail,
+
+    // Auth errors.
+    AuthFailNoAuthTokenCookie,
+    AuthFailTokenWrongFormat,
+    AuthFailCtxNotInRequestExt,
+
+    // Model errors.
+    TicketDeleteFailIdNotFound { id: u64 },
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        println!("--> {:<12} - {self:?}", "INTO_RES");
+
+        // Create a placeholder Axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response.
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // Auth.
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+
+            // Model.
+            Self::TicketDeleteFailIdNotFound { id } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            
+            // Fallback.
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
---
+++ src/main.rs
@@ -0,0 +1,120 @@
+#![allow(unused)]
+
+use crate::log::log_request;
+use crate::model::ModelController;
+
+pub use self::error::{Error, Result};
+
+use std::net::SocketAddr;
+
+use axum::http::{Uri, Method};
+use axum::{Router, middleware, Json};
+use axum::extract::{Query, Path};
+use axum::routing::{get, get_service};
+use axum::response::{Html, IntoResponse, Response};
+use ctx::Ctx;
+use serde::Deserialize;
+use serde_json::json;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+use uuid::Uuid;
+
+mod ctx;
+mod error;
+mod log;
+mod model;
+mod web;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mc = ModelController::new().await?;
+
+    let routes_apis = web::routes_tickets::routes(mc.clone())
+        .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+
+    let routes_all = Router::new()
+        .merge(routes_hello()
+        .merge(web::routes_login::routes())
+        .nest("/api", routes_apis) 
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(mc.clone(), web::mw_auth::mw_ctx_resolver))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static()));
+
+    // region:  --- Start Server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("--> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+    // endregion: --- Start server
+    Ok(())
+}
+
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
+    println!("->> {:<12} - main_response_mapper", "HANDLER");
+    let uuid = Uuid::new_v4();
+
+    // Get the eventual response error.
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // If client error, build the new response.
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // Build the new response from the client_error_body.
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // Build and log the server log line.
+    let client_error = client_status_error.unzip().1;
+    log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+    
+    println!();
+    error_response.unwrap_or(res)
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}!!!</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}!!!</strong>"))
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
